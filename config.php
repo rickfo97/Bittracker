1c2bdc3e9d9497b4dd90263c72e3cd283fcb61ec
Hannes Kindströmmer <hannes@kindstrommer.se>
2017-03-26 20:35:51 +0200

    Make config more SVN friendly
    Signed-off-by: Hannes Kindströmmer <hannes@kindstrommer.se>

--- config.php
+++ config.php
@@ -2,29 +2,15 @@
 class Config
 {
 
-    private static $config = array(
-        'db_driver' => 'mysql',
-        'db_host' => 'localhost',
-        'db_name' => 'tracker',
-        'db_user' => 'root',
-        'db_password' => '',
-
-        'numwant_max' => 100,
-        'announce_interval' => (90 * 60),
-        'announce_interval_min' => (60 * 60),
-        'open_track' => true,
-        'expire_interval' => '2 HOUR',
-        'save_stats' => false,
-        'full_scrape' => false,
-
-        'log_file' => 'log.txt',
-        'time_format' => 'Y-m-d H:i:s'
-    );
+    private static $config;
 
     public static function get($key)
     {
-        if (key_exists($key, self::$config)) {
-            return self::$config[$key];
-        }
+      if(is_null(self::$config)){
+        self::$config = parse_ini_file('config.ini');
+      }
+      if (key_exists($key, self::$config)) {
+          return self::$config[$key];
+      }
     }
 }
