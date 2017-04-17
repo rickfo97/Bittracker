078d98f492620dce27609ca8f747d4ad4230e1e8
Rickardh Forslund <rickfo_97@hotmail.com>
2017-04-17 22:15:10 +0200

    Config changes
    Can now write to ini file and if it can't find 'config.ini' it will try
    with 'config.example.ini'

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -6,14 +6,38 @@ class Config
 {
 
     private static $config;
+    private static $configFile;
 
     public static function get($key)
     {
         if(is_null(self::$config)){
-            self::$config = parse_ini_file('config.ini');
+            self::$configFile = file_exists('config.ini') ? 'config.ini' : 'config.example.ini';
+            self::$config = parse_ini_file(self::$configFile, false, INI_SCANNER_TYPED);
         }
         if (key_exists($key, self::$config)) {
             return self::$config[$key];
         }
+    }
+
+    public static function change($key, $value)
+    {
+        if(is_null(self::$config)){
+            self::$configFile = file_exists('config.ini') ? 'config.ini' : 'config.example.ini';
+            self::$config = parse_ini_file(self::$configFile, false, INI_SCANNER_TYPED);
+        }
+        if (key_exists($key, self::$config)) {
+            self::$config[$key] = $value;
+            return true;
+        }
+        return false;
+    }
+
+    public static function save()
+    {
+        $file = file_get_contents(self::$configFile);
+        foreach (self::$config as $key => $value){
+            $file = preg_replace("/$key=.*/", "$key=" . var_export($value, true), $file, 1);
+        }
+        return file_put_contents(self::$configFile, $file, LOCK_EX);
     }
 }
